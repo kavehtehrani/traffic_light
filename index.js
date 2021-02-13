68434bfb013cc66fedf1c596efd54aca1e3d18e6
Kaveh <kaveh_tehrani@yahoo.com>
2021-02-13 00:14:48 -0500

    modified:   index.js 	modified:   views/home.ejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const ejsMate = require('ejs-mate');
 const path = require('path');
 const Traffic = require('./models/traffic');
 const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
 
 
 mongoose.connect('mongodb://localhost:27017/traffic_light', {
@@ -23,8 +25,10 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -36,6 +40,18 @@ app.post('/', async (req, res, next) => {
     res.redirect('/');
 });
 
+app.get('/', async (req, res) => {
+    const listName = await Traffic.find({});
+    res.render('home.ejs', {listName})
+});
+
+app.delete('/:id', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    await Traffic.findByIdAndDelete(id);
+    res.redirect('/');
+}))
+
+
 // ***TEST DATA***
 // app.get('/booboo', async (req, res) => {
 //     const traffic = new Traffic({ name: 'christie', status: '2' })
@@ -43,9 +59,9 @@ app.post('/', async (req, res, next) => {
 //     res.send(traffic)
 // });
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+// app.all('*', (req, res, next) => {
+//     next(new ExpressError('Page Not Found', 404))
+// })
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
