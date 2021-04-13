cca23a112df5e86f952fadf9851fe1df1532f729
Kaveh <kaveh_tehrani@yahoo.com>
2021-04-13 16:20:58 -0400

    heroku

--- public/javascript/recaptcha.js
+++ public/javascript/recaptcha.js
@@ -1,17 +1,19 @@
 
 module.exports.checkHuman = async (req, res, next) => {
-    const secret_key = process.env.CAPTHA_API_SECRET_V3;
-    const token = req.body.token;
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
-    const ret_val = await fetch(url, {method: 'POST', headers: {'Content-Type': 'application/json'}});
-    const data = await ret_val.json();
-
-    if (data.score < .5) {
-        console.log('BOT DETECTED')
-        req.flash('error', 'Bot detected. Please refresh page and retry.')
-        res.redirect('/')
-    }
-    else{
-        next();
-    }
+    // const secret_key = process.env.CAPTHA_API_SECRET_V3;
+    // const token = req.body.token;
+    // const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
+    // const ret_val = await fetch(url, {method: 'POST', headers: {'Content-Type': 'application/json'}});
+    // const data = await ret_val.json();
+    //
+    // if (data.score < .5) {
+    //     console.log('BOT DETECTED')
+    //     req.flash('error', 'Bot detected. Please refresh page and retry.')
+    //     res.redirect('/')
+    //     next();
+    // }
+    // else{
+    //     next();
+    // }
+    next();
 }
