7e7bcca9078c1c293066098fc07f8778275adcf8
Kaveh <kaveh_tehrani@yahoo.com>
2021-05-17 23:34:26 -0400

    css styling

---
+++ public/javascript/trafficInstance.js
@@ -0,0 +1,18 @@
+function copyLink() {
+    var copyText = document.getElementById("sharelink");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand("copy");
+    // req.flash('success', 'Copied'   );
+    // alert("Copied");
+    document.getElementById('copied').style.display = "block";
+}
+
+function closebutton() {
+    var close = document.getElementById("copied");
+    if (close.style.display === "none") {
+        close.style.display = "block";
+    } else {
+        close.style.display = "none";
+    }
+}
